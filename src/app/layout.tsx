b08b95f4a1d23a7e96382c39c5f38f7b0f57b90d
Gino <ginoalodar@gmail.com>
2024-08-14 21:36:00 -0500

    fix: modified layout.tsx to get the textured pattern to fill the full screen in every page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="text-slate-100 dark:text-slate-900">
-      <body className={`${nunitoSans.className} ` }>
+      <body className={`${nunitoSans.className} bg-midnightblue dark:bg-regblue ` }>
+        <div className="pointer-events-none fixed inset-0 -z-40 h-full w-full bg-[url('/textures/grainy13.png')] dark:bg-[url('/textures/textureVein5.png')] opacity-20 mix-blend-soft-light"></div>  
+        <div className="background-gradient fixed inset-0 -z-50 w-full h-full" />
         <Header />
-        <div className="bg-midnightblue dark:bg-regblue flex flex-col justify-center">
-          <div className="">
-            {children}  
-          </div>
+        <div className="flex flex-col justify-center">
+          {children}
           <Footer />
         </div>
       </body>
