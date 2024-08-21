095515d26d8b751e21531acde28334f38690e061
Gino <ginoalodar@gmail.com>
2024-08-20 23:27:00 -0500

    feat: added monorama font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfolio Gino Odar",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="text-slate-100 dark:text-slate-900">
-      <body className={`${nunitoSans.className} bg-midnightblue dark:bg-regblue flex flex-col min-h-screen relative` }>
+      <body className={`bg-midnightblue dark:bg-regblue flex flex-col min-h-screen relative` }>
         <div className="pointer-events-none fixed inset-0 -z-40 h-full w-full bg-[url('/textures/grainy13.png')] dark:bg-[url('/textures/textureVein5.png')] opacity-20 mix-blend-soft-light"></div>  
         <div className="background-gradient fixed inset-0 -z-50 w-full h-full" />
         <Header />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { createClient } from '@/prismicio';
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
 
-const Footer = () => {
+async function Footer(){
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+
   return (
     <div className='min-h-[5vh] bg-slate-950 bg-opacity-25 mt-auto'>
         <div className='flex justify-between p-4'>
-          <p>things</p>
+          <div className="flex items-center gap-2">
+            <Link
+              href="/"
+              className="text-xl font-extrabold tracking-tighter text-slate-100 transition-colors duration-150 hover:text-chilli"
+            >
+              {settings.data.name}
+            </Link>
+            <p>|</p>
+            <p className=" text-sm text-slate-300 ">
+              © {new Date().getFullYear()} {settings.data.name}
+            </p>
+          </div>
           <p>things2</p>
         </div>
     </div>
