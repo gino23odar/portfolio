fb95b0058e77b43799911b1ba362c9af705ecb35
Gino <ginoalodar@gmail.com>
2024-08-25 23:18:01 -0500

    feat: included animation and temp fix to the resume link in About me page

--- src/slices/AboutMe/index.tsx
+++ src/slices/AboutMe/index.tsx
@@ -1,11 +1,12 @@
+'use client'
+
+import React, {useRef } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
-import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
 import Bounds from "@/components/Bounds";
 import Heading from "@/components/Heading";
 import Avatar from "@/components/Avatar";
 import ShuffleHeader from "@/components/ShuffleHeader";
-import PDFDownload from "@/components/PDFDownload";
 
 
 /**
@@ -17,6 +18,21 @@ export type AboutMeProps = SliceComponentProps<Content.AboutMeSlice>;
  * Component for "AboutMe" Slices.
  */
 const AboutMe = ({ slice }: AboutMeProps): JSX.Element => {
+
+  const diagRef = useRef<HTMLDialogElement>(null);
+
+  const showModal = () =>{
+    if(diagRef.current){
+      diagRef.current.showModal();
+    }
+  };
+
+  const hideModal = () =>{
+    if(diagRef.current){
+      diagRef.current.close();
+    }
+  };
+
   return (
     <Bounds
       data-slice-type={slice.slice_type}
@@ -30,18 +46,34 @@ const AboutMe = ({ slice }: AboutMeProps): JSX.Element => {
         <div className='prose prose-xl prose-slate prose-invert col-start-1'>
           <PrismicRichText field={slice.primary.description} />
         </div>
+        <dialog ref={diagRef} className="border-4 border-chilli">
+          <h2 className="text-xl p-4 ">NOTICE:</h2>
+          <div className="flex flex-col justify-center p-4"> 
+            <p>Due to a scrapper spamming this, I decided to temporarily take down </p>
+            <p>the pdf download coponent with @react-pdf/renderer.</p>
+            <p>Sorry for the inconvenience.</p>
+            <div className="flex justify-center items-center gap-2 mt-2">
+              <p>Feel free to contact me for a resume here: </p>
+              <a href="./contact" className="bg-midnightblue text-white p-1 "> CONTACT </a>
+            </div>
+          </div>
+          <form method="dialog" className="flex justify-center p-4">
+            <button onClick={hideModal} className="bg-coolgray p-2">close</button>
+          </form>
+        </dialog>
+        <button
+            //linkField={slice.primary.button_link}
+            onClick={showModal}
+            id="resume-modal"
+            className="lg:m-10 xl:mx-20">
+            {slice.primary.button_text}
+        </button>
         <Avatar
           image={slice.primary.avatar}
           className="row-start-1 max-w-3xl md:col-start-2 md:row-end-3 xl:row-end-4"
         />
       </div>
       
-      {/* <button
-          //linkField={slice.primary.button_link}
-          className="lg:m-10 xl:m-12">
-          {slice.primary.button_text}
-      </button> */}
-      <PDFDownload/>
     </Bounds>
   );
 };
